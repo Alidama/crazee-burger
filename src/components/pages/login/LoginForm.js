0cdd3143a27c080c1d40ffc753d5a437e64882f8
Ali Dama <alicharafdama@gmail.com>
2023-09-12 15:59:09 +0200

    F01: LOGIN > Add LoginForm

---
+++ src/components/pages/login/LoginForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+
+function LoginForm() {
+  // state
+
+  const [inputValue, setInputValue] = useState("");
+
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+  };
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // affichage
+
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez vous</h2>
+      <input
+        onChange={handleChange}
+        value={inputValue}
+        type="text"
+        placeholder="Entrez votre prénom..."
+        required
+      />
+      <button>Accéder à votre espace</button>
+    </form>
+  );
+}
+
+export default LoginForm;
