e8a178e1aaddbb0d266a10d4c3baf1c14e258d8f
Ali Dama <alicharafdama@gmail.com>
2023-09-12 14:15:40 +0200

    F01 : LOGIN > Add LoginPage

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  // state
+
+  const [inputValue, setInputValue] = useState("");
+
+  // comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+  };
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // affichage
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={inputValue}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
