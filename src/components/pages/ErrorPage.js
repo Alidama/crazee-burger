440f55baa1b06212384741cb5af1af5169d15775
Ali Dama <alicharafdama@gmail.com>
2023-09-15 17:33:30 +0200

    F02: LOGIN > Add redirection from/to ErrorPage

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
     <div>
       <h1>ErrorPage</h1>
+      <Link to="/">
+        <button>Retourner à la page d'accueil</button>
+      </Link>
     </div>
   );
 }
